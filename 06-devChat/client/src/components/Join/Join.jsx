4798d93ae3be249ace7001ed8eaa661412b2e271
Hillary <hillarycacheiro98@gmail.com>
2026-04-23 14:43:05 -0300

    Funcionalidades: Implementar componentes de Chat e Join com integração ao socket.io e estilização aprimorada

--- 06-devChat/client/src/components/Join/Join.jsx
+++ 06-devChat/client/src/components/Join/Join.jsx
@@ -1,10 +1,10 @@
-import { useRef } from "react"
-import style from "./Join.module.css"
+import { useRef } from "react";
+import style from "./Join.module.css";
 
-import {Input, Button} from "@mui/material"
+import { Input, Button } from "@mui/material";
 import io from "socket.io-client";
 
-const Join = () => {
+const Join = (props) => {
   const usernameRef = useRef();
 
   const handleSubmit = async () => {
@@ -12,23 +12,39 @@ const Join = () => {
     if (!username.trim()) return;
 
     const socket = io.connect("http://localhost:3001");
-    socket.emit("set_username", username)
+    socket.emit("set_username", username);
 
+    props.setChatVisibility(true);
   };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSubmit();
+    }
+  };
   return (
-    <div>
-        <h2>Bem-Vindo ao devChat</h2>
-        <Input inputRef={usernameRef} placeholder="Nome de usuário" />
-        <Button 
-        sx={{mt:2, mb:2}} 
-        variant="contained" 
-        onClick={() => handleSubmit()}
-        >
-            Entrar 
-        </Button>
-    </div>
-  )
-}
+    <>
+      <div className={style.dev_logo}>
+        <img src="" alt="Logo do DevChat" />
+      </div>
 
-export default Join
+      <div className={style.join_container}>
+        <h2>Bem-vindo ao devChat!</h2>
+        <Input
+          inputRef={usernameRef}
+          placeholder="Nome de usuário"
+          onKeyDown={handleKeyPress}
+        />
+        <Button
+          sx={{ mt: 2, mb: 2 }}
+          variant="contained"
+          onClick={() => handleSubmit()}
+        >
+          Entrar
+        </Button>
+      </div>
+    </>
+  );
+};
+
+export default Join;
